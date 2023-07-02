8041e07f7df28b851c65170fe3247568a9a32a60
DenZhigalov <denzhigalov@contorra.com>
2023-07-02 10:22:24 +0700

    moved built file

---
+++ .build/test/test.lua
@@ -0,0 +1,19 @@
+-- IMPORT LOW/CLASS
+function Class(parent) 	local class = {} 	local mClass = {} 	class.__index = class 	mClass.__index = parent 	class.Super = parent 	function mClass:__call(...) 		local instance = setmetatable({}, class) 		if type(class.init) == "function" then 			return instance, instance:init(...) 		end 		return instance 	end 	return setmetatable(class, mClass) end
+
+-- IMPORT LOW/EXAMPLECLASS
+ExampleClass = Class()
+
+function ExampleClass:init(I)
+	self.I = I
+end
+
+function ExampleClass:foo()
+	self.I:LogToHud("Hello World!")
+end
+
+-- IMPORT TEST/TEST
+function Update(I)
+	local exampleObject = ExampleClass(I)
+	exampleObject:foo()
+end
